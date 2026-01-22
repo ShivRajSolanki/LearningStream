4949b56a05c4fe333eedcd77cb773ea1a65cd8e8
ShivRajSolanki <shivrajsolanki6001@gmail.com>
2026-01-22 20:31:24 +0530

    Day 17

---
+++ sumOfAllEven.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class sumOfAllEven {
+    public static void main(String[] args) {
+         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6);
+
+        int sum = list.stream()
+                      .filter(n -> n % 2 == 0)
+                      .mapToInt(Integer::intValue)
+                      .sum();
+
+        System.out.println(sum);
+    }
+}
+
